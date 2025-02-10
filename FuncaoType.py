55c45e636895ffb2a09c333a482d3e79a2555465
Eberrth44 <eberthsilva011@gmail.com>
2025-02-10 19:27:11 -0300

    classificaçao de variaveis
    variaveis nomeadas apos a execuçao

---
+++ FuncaoType.py
@@ -0,0 +1,11 @@
+faturamento =  1000
+print ("Para o faturamento que recebeu 1000 tipo é:", type(faturamento))
+faturamento =  1000.00
+print ("Para o faturamento que recebeu 1000.00 tipo é:", type(faturamento))
+
+faturamento =  "1.000"
+print ('Para o faturamento que recebeu "1.000" tipo é:', type(faturamento))
+ 
+ganha_bonus = True 
+print ('Para variavel ganaha_bonus que recebeu "true" o tipo é:', type(ganha_bonus))
+ 
