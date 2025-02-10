cc0838d7d8f19c4e5587aa3b3641c9e71365e123
Eberrth44 <eberthsilva011@gmail.com>
2025-02-10 20:44:25 -0300

    usando metodo len
    contado de variavel aplicada

--- StringTexto.py
+++ StringTexto.py
@@ -1,6 +1,9 @@
-email = 'EberthSilva@gmail.com'
+email = 'EberthSilva@gmail.commmm'
 
 print ("\nPegando posição ZERO nom email atribuido:")
 print (email[0])
 print ("\nPegando da posição ZERO até a ONZE do email atribuido:")
-print(email[0:11])
+print (email[0:12])
+#usando o metodo len para saber o tamnaho de um texto
+print ("\nUsando metodo LEN para saber o tamanho da variavel email:")
+print (len(email))
