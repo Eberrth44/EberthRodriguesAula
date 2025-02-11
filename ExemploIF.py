1df451145d6a986ecaa5310610bdbbc6d18cc583
Eberrth44 <eberthsilva011@gmail.com>
2025-02-11 19:17:33 -0300

    maior e menor ou igual
    #verificador de numero digitado se é maior ou menor que x

--- ExemploIF.py
+++ ExemploIF.py
@@ -1,8 +1,8 @@
-numero = 10
-numero = input ("Digite um numero:")
-if numero > 10:
-    print ("seu numero é menor que 10")
-elif:
-    print ("ele é maior que 10")
-else :
-    print ("seu numero é igual a 10")
+#verificador de numero digitado se é maior ou menor que x
+n2 = int ((input) ("Digite um numero:"))
+if n2 < 10:
+    print ("seu numero é menor que 10:",n2)
+elif n2 == 10:
+    print ("Seu numero é igual a 10:", n2)
+else:
+    print ("seu numero é maior que 10:",n2)
