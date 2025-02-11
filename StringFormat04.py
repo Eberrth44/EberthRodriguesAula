1670f59b523f8d0212e5e407549211013f9fb73e
Eberrth44 <eberthsilva011@gmail.com>
2025-02-10 22:05:20 -0300

    varios metodos
    uso de varios metodos de variaveis

--- StringFormat04.py
+++ StringFormat04.py
@@ -2,3 +2,8 @@ faturamento = 2000
 custo = 500
 lucro = faturamento - custo
 print ("O faturamento da loja foi de: {0}, o custo foi de: {1} e o lucro foi de: {2}".format(faturamento, custo, lucro))
+
+#uso do IN
+#retorna TRUE se o arquivo estiver CONTIDA NA SEQUEMCIA
+email = ' EberthSilva@gmail.com'
+print ("@" in "Eberth@gamil.com") #verificar somente nessa linha se exeiste o (@)
---
+++ StringMaisMetodos.py
@@ -0,0 +1,73 @@
+#TRANSFORMAR APENAS A PRIMEIRA LETRA DE UMA STRING EM MMAIUSCULA
+nome = "eberth"
+print (nome.capitalize(),"\n")
+print ("--------------------------------------------")
+
+
+#TRANFORMA TODAS AS LETRAS EM MINUSCULA
+nome = "EBERTH"
+print (nome.casefold(),"\n")
+print ("--------------------------------------------")
+
+
+#CONTA O NUMERO DE VEZES QUE UM CARACTERE ESPECIFICO APARECE NA STRING 
+email = "EberthSilva@gamail.com"
+print (email.count('i'), "\n")
+print ("--------------------------------------------")
+
+
+#RETORNA TRUE (veradeiro) OU false (falso) PARA UM TESTE se A STRING TERMINA COM UMA STRING 
+nome = "EberthSilva@gmail.com"
+print (nome.endswith('gamil.com'),"\n")
+print ("--------------------------------------------")
+
+
+#ENCONTRA A POSIÇAO DO TERMO PROCURADO. LEMBRE-SE COMEÇA DO zero
+nome = "Eberthsilva@gmail.com"
+print (nome.find('@'),"\n")
+print ("--------------------------------------------")
+
+
+#VERIFICA SE O TEXTO É (TODO) COM LETRAS
+nome = "Eberth"
+print (nome.isalpha(),"\n")
+print ("--------------------------------------------")
+
+
+#VERIFICA SE TODO O TEXTO E COM NUMEROS
+nome = "123"
+print (nome.isnumeric(),"\n")
+print ("--------------------------------------------")
+
+
+#SUBSTITUI UM CARATERE ESCOLHIDO POR OUTRO.
+nome = "Eberth"
+print (nome.replace('o','u'),"\n")
+print ("--------------------------------------------")
+
+
+#SEPARA O TEXTO string BASEADO EM ALGUM CARACTERE INDICADO
+nome = "Eberth @ Silva Rodrigues"
+print (nome.split('@'), "\n")
+print ("--------------------------------------------")
+
+
+#COLOCAR TODAS AS LETRAS INICIADAS EM MAISCULAS. 
+nome = "eberth da silva rodrigues"
+print (nome.title(),"\n")
+print ("--------------------------------------------")
+
+
+#RETIRA OS CARACTERES INDESEJADOS, COMO POR EXEMPLO espaços QUE NÃO AGREGAM VALOR   
+nome = "    eberth silva rodrigues"
+print (nome.strip(),"\n")
+print ("--------------------------------------------")
+
+#RETORNA true OU false PARA UM TESTE DE UMA STRING COM TEXTO ESPECIFICO
+nome = "eberth 2008"
+print (nome.startswith("ebe"),"\n")
+print (nome.startswith("Ebe"),"\n")
+print ("--------------------------------------------")
+
+
+#
