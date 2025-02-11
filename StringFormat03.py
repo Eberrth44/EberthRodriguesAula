9209555d749400775b97a6a6b574f01499fd2577
Eberrth44 <eberthsilva011@gmail.com>
2025-02-10 21:12:53 -0300

    usando format
    para fazer subtraçao

---
+++ StringFormat03.py
@@ -0,0 +1,4 @@
+faturamento = 2000
+custo = 50 
+lucro = faturamento
+print ("O faturamento da loja foi de:", lucro)
---
+++ StringFormat04.py
@@ -0,0 +1,4 @@
+faturamento = 2000
+custo = 500
+lucro = faturamento - custo
+print ("O faturamento da loja foi de: {0}, o custo foi de: {1} e o lucro foi de: {2}".format(faturamento, custo, lucro))
