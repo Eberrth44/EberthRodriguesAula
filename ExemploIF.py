c105415f6c058241faa47702184e9d829c8a97a6
Eberrth44 <eberthsilva011@gmail.com>
2025-02-10 22:17:38 -0300

    maior e menor
    descobrir se o numero é maior ou menor que x

---
+++ ExemploIF.py
@@ -0,0 +1,8 @@
+numero = 10
+numero = input ("Digite um numero:")
+if numero > 10:
+    print ("seu numero é menor que 10")
+elif:
+    print ("ele é maior que 10")
+else :
+    print ("seu numero é igual a 10")
