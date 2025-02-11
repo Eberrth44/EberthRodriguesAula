ce619e586bac8f817c9d8869bbf3e29c753ca4e1
Eberrth44 <eberthsilva011@gmail.com>
2025-02-11 19:49:13 -0300

    IF Ellif e Else
    #faça um programa uqe o usuario digite um numero e diga se o numero é superior a 20, inferior a ou se esta entre 10 e 20.

---
+++ IFeElse01.py
@@ -0,0 +1,9 @@
+#faça um programa uqe o usuario digite um numero e diga se o numero é superior a 20, inferior a ou se esta entre 10 e 20.
+numero = int (input ("Digite um numero: "))
+
+if numero > 20:
+    print ("seu numero é maior que 20")
+elif numero < 10:
+    print ("seu numero é menor que 10")
+else:
+    print ("seu numero esta entre 10 e 20")
