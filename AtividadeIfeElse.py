57fa3aa8874c97bd92df71b1f6df37fe51a98955
Eberrth44 <eberthsilva011@gmail.com>
2025-02-11 20:01:19 -0300

    atividade if e else
    verificador duplu de if

---
+++ AtividadeIfeElse.py
@@ -0,0 +1,7 @@
+nota = int (input ("Digite a nota do aluno:"))
+if nota >=7:
+    print ("aprovado")
+else:
+    if nota >= 10:
+        print ("Não aprovado/ Recuperação")
+    print ("nao aprovado/ reprovado direto")
