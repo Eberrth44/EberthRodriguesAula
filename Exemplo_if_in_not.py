7577c545a5985165fc31109bb9a40d64f9fe9b0e
Eberrth44 <eberthsilva011@gmail.com>
2025-02-11 21:05:29 -0300

    verificador de variavel
    verifica de a variavel escolhida esta em uso

---
+++ Exemplo_if_in_not.py
@@ -0,0 +1,7 @@
+if '#' in 'eberthsilva011@gamail.com':
+    print ('Nao tem o #')
+else:
+    print('tem o #')
+
+
+
---
+++ IF_in.py
@@ -0,0 +1,4 @@
+if '@' in 'eberthsilva011@gamail.com':
+    print ('o email é valido')
+else:
+    print('O email nao é valido')
