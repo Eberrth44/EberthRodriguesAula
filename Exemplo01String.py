b284125ae8bf781f2f115e8cf760e9901e98bb90
Eberth_4 <eberthsilva011@gmail.com>
2025-02-12 19:12:53 -0300

    exercicios

--- Exemplo01String.py
+++ Exemplo01String.py
@@ -1,12 +1,15 @@
 #correçao de variaveis 
 nome = "ada lovelace"
 print (nome.title())
+print("------------------")
 
 nome = "Ada Lovelace"
 #Nome maiusculo
 print (nome.upper())
+print("------------------")
 # nome minusculos
 print (nome.lower())
+print("------------------")
 #soma de nome
 primeiro_nome = "ada"
 sobrenome = "lovelace"
