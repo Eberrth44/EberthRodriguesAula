b1cfc46e378090d0b8d3e07e8d07b225f3434313
Eberrth44 <eberthsilva011@gmail.com>
2025-02-19 19:25:28 -0300

    segurança

--- EXEMPLOS_FOR.py/EXEMPLOS_TUPLAS.py/tuplasUNPANCKING.py
+++ EXEMPLOS_FOR.py/EXEMPLOS_TUPLAS.py/tuplasUNPANCKING.py
@@ -1,5 +1,5 @@
 #metodo facil 
-vendas = ("liria", "08/09/2008", "19/02/2025", 2000, "estagiario", "nada")
+vendas = ("liria", "08/09/2008", "19/02/2025", 2000, "estagiario")
 nome, data_nascimento, data_contratacao, salario,cargo= vendas
 print (nome)
 print (data_nascimento)
