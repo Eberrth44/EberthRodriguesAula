72a4c88835013359f98d726fe097042b44c07903
Eberrth44 <eberthsilva011@gmail.com>
2025-02-19 19:18:50 -0300

    TUPLAS
    exemplo de tuplas unpancking

---
+++ EXEMPLOS_FOR.py/EXEMPLOS_TUPLAS.py/tuplasUNPANCKING.py
@@ -0,0 +1,8 @@
+#metodo facil 
+vendas = ("liria", "08/09/2008", "19/02/2025", 2000, "estagiario", "nada")
+nome, data_nascimento, data_contratacao, salario,cargo= vendas
+print (nome)
+print (data_nascimento)
+print (data_contratacao)
+print (salario)
+print (cargo)
